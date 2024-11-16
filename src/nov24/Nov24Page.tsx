a47115c56a84fb0f6cf06e44f5761917196246fb
Liri S <liri@ownid.com>
2024-11-16 20:41:23 +0200

    Working on nov24

--- src/nov24/Nov24Page.tsx
+++ src/nov24/Nov24Page.tsx
@@ -1,7 +1,6 @@
 import styled, {css} from "styled-components";
-import {MARKER_4, PANE_COLOR, SEA_COLOR} from "../data/colors";
-import {Link} from "react-router-dom";
-import {Proposition5Book2V1} from "../sep24/Proposition5Book2V1";
+import {MARKER_4, MARKER_5, PANE_COLOR, SEA_COLOR} from "../data/colors";
+import {Link, useNavigate} from "react-router-dom";
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -12,6 +11,7 @@ const Wrapper = styled.div`
 
 const Container = styled.div`
     display: flex;
+    min-height: calc(100vh - 8rem);
     flex-direction: column;
     align-items: center;
     justify-content: start;
@@ -47,10 +47,13 @@ const LinkStyle = css`
         font-size: 1.2rem;
     }
     color: ${MARKER_4};
+    cursor: pointer;
 `
 
 const StyledAnchor = styled.a`
     ${LinkStyle};
+    color: ${MARKER_5};
+    font-size: 1.8rem;
 `;
 
 const StyledLink = styled(Link)`
@@ -65,7 +68,16 @@ const Block = styled.div`
     gap: 0.5rem;
 `
 
+const Tile = styled.img`
+    height: auto;
+    width: auto;
+    max-width: min(22rem, 92vw);
+    cursor: pointer;
+`
+
 export const Nov24Page = () => {
+    const navigate = useNavigate();
+    const pdf = "/presentation/FrenchTranslationsOfEuclidsElementsInTheFirstHalfOfThe17thCentury.pdf"
     return (
         <Wrapper>
             <Container>
@@ -74,16 +86,21 @@ export const Nov24Page = () => {
                 <div>Mia Joskowicz, November 2024</div>
                 <div/>
                 <div/>
-                <StyledAnchor href="/presentation/TransformationOfMathematicalKnowledge.pdf" target="_blank" rel="noopener noreferrer">Presentation Slides</StyledAnchor>
+                <StyledAnchor href={pdf} target="_blank" rel="noopener noreferrer">Presentation Slides</StyledAnchor>
+                <Tile src="/presentation/nov24.png" onClick={() => window.open(pdf, "_blank")?.focus()}/>
                 <div/>
                 <div/>
                 <Block>
                     <StyledLink to="/">Elements Timeline Map</StyledLink>
+                    <Tile src="/presentation/map.png" onClick={() => navigate('/')}/>
                     <div>Best viewed on desktop</div>
                 </Block>
                 <div/>
                 <div/>
-                <Proposition5Book2V1/>
+                <Block>
+                    <StyledLink to="/sep24">Transformation of Mathematical Knowledge: German Editions of Euclid’s Elements in the 16th-17th Centuries</StyledLink>
+                    <div>Talk, September 2024</div>
+                </Block>
             </Container>
         </Wrapper>
     );
